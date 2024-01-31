c2270a953947a3182e808281ec465130691e0b16
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-31 12:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+ const timerElement = document.getElementById('timer');
+
+function updateTimer() {
+            const now = new Date();
+            const date = now.toLocaleDateString();
+            const time = now.toLocaleTimeString();
+
+    timerElement.textContent = `${date} ${time}`;
+        }
+
+        updateTimer();
+        setInterval(updateTimer, 1000);
